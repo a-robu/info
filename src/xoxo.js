022b1da5f4071b073e04cf75dae48f6fb3f92137
Andrei Robu <andrei.rocks.1992@gmail.com>
2016-07-20 19:43:56 +0100

    make x() encoded more human readable

--- src/xoxo.js
+++ src/xoxo.js
@@ -2,6 +2,12 @@ const ExtendableError = require('es6-error')
 
 class CannotSerializeType extends ExtendableError {}
 
+const TYPES = {
+    JSON_FRIENDLY: 'j',
+    ARRAY: 'a',
+    SET: 's'
+}
+
 const JSON_FRIENDLY_TYPES = [
     'string', 'number'
 ]
@@ -10,15 +16,15 @@ function serialize(obj) {
     let type
     let for_json
     if (obj instanceof Array) {
-        type = 'Array'
+        type = TYPES.ARRAY
         for_json = obj.map(serialize)
     }
     else if (obj instanceof Set) {
-        type = 'Set'
+        type = TYPES.SET
         for_json = Array.from(obj).map(serialize).sort()
     }
     else if (JSON_FRIENDLY_TYPES.includes(typeof obj)) {
-        type = 'json_friendly'
+        type = TYPES.JSON_FRIENDLY
         for_json = obj
     }
     else throw new CannotSerializeType(`typeof: ${typeof obj}.`)
@@ -28,11 +34,11 @@ function serialize(obj) {
 function unserialize(str) {
     let [type, from_json] = JSON.parse(str)
     switch (type) {
-        case 'Array':
+        case TYPES.ARRAY:
             return from_json.map(unserialize)
-        case 'Set':
+        case TYPES.SET:
             return new Set(from_json.map(unserialize))
-        case 'json_friendly':
+        case TYPES.JSON_FRIENDLY:
             return from_json
         default:
             throw `Unhandled type: ${type}`
